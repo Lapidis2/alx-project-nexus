7f00788bad8f80e75dcc49de4c051a39a7a0c169
Lapidis2 <hitlapidis2@gmail.com>
2025-09-15 15:53:17 +0200

    project scaffolding

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+  return (
+	<div>
+	  <h1 className='bg-red-600 h-full w-full'>Welcome to my ecommerce</h1>
+	</div>
+  )
+}
+
+export default index
