60bea2f4f4bb8a21fe9a65be2af5822fac19187c
Lapidis2 <hitlapidis2@gmail.com>
2025-09-21 22:12:04 +0200

    update mock data

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -4,6 +4,7 @@ const products = [
 	{ id: "1", name: "HeadPhones", price: 1200, image: "/assets/images/products/headphone.jpg", description: "High-end laptop", quantity: 10, Vendor: { storeName: "Tech Store" } },
 	{ id: "2", name: "Phone", price: 800, image: "/assets/images/products/phone.png", description: "Latest smartphone", quantity: 15, Vendor: { storeName: "Phone World" } },
 	{ id: "3", name: "Tablet", price: 500, image: "/assets/images/products/shoes.jpg", description: "Lightweight tablet", quantity: 8, Vendor: { storeName: "Gadget Shop" } },
+	{ id: "4", name: "Phone", price: 800, image: "/assets/images/products/phone.png", description: "Latest smartphone", quantity: 15, Vendor: { storeName: "Phone World" } }
   ];
   
 
