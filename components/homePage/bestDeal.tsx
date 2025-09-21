565ed01d94bf3f884cf4bde275f94d6f15652d6f
Lapidis2 <hitlapidis2@gmail.com>
2025-09-21 22:28:49 +0200

    remove unnecessary comments

--- components/homePage/bestDeal.tsx
+++ components/homePage/bestDeal.tsx
@@ -16,14 +16,14 @@ const bestDealsData: Product[] = [
     name: "Samsung TV",
     description: "Discover the latest Samsung TV deals at unbeatable prices.",
     price: "678,453",
-    image: "/assets/images/Products/tv.png", 
+    image: "/assets/images/products/tv.png", 
   },
   {
     id: "2",
     name: "Sony Headphones",
     description: "Experience immersive sound with the latest Sony headphones.",
     price: "123,000",
-    image: "/assets/images/Products/headphone.png",
+    image: "/assets/images/products/headphone.png",
   },
 ];
 
@@ -44,8 +44,7 @@ const BestDeals: React.FC = () => {
 
   return (
     <section className="bg-primary text-white p-8 sm:p-16 md:pl-24 flex flex-col md:flex-row items-center gap-10 font-outfit min-h-screen">
-      
-      {/* Text Content */}
+
       <article className="md:w-1/2 mb-8 md:mb-0 md:mr-8">
         <header>
           <h2 className="text-2xl sm:text-xl md:text-3xl font-bold mb-4 md:mb-8">
@@ -66,7 +65,6 @@ const BestDeals: React.FC = () => {
         </button>
       </article>
 
-      {/* Image Content */}
       <figure className="md:w-1/2 relative w-full h-[300px] sm:h-[350px] md:h-[400px] lg:h-[450px]">
         {loading && <Skeleton height="100%" width="100%" className="rounded-lg" />}
         <Image
