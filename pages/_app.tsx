9062a0f8259b7ca18a1fba561234e28c77de9f9e
Lapidis2 <hitlapidis2@gmail.com>
2025-09-25 10:40:41 +0200

    editing main app so as to use providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,51 @@
+
+
 import { useRouter } from 'next/router';
 import { useEffect, useState, ReactNode } from 'react';
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
+
+
+import { AuthProvider } from 'react-auth-kit'; 
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const [layout, setLayout] = useState<React.FC<{ children: ReactNode }> | null>(null);
+  const [Layout, setLayout] = useState<React.FC<{ children: ReactNode }> | null>(null);
+  const [loadingLayout, setLoadingLayout] = useState(true);
 
   useEffect(() => {
+    setLoadingLayout(true);
     if (router.pathname.startsWith('/admin')) {
-      import('@/components/dashboard/AdminLayout').then((mod) => setLayout(() => mod.default));
+      import('@/components/dashboard/AdminLayout')
+        .then((mod) => setLayout(() => mod.default))
+        .finally(() => setLoadingLayout(false));
     } else if (router.pathname.startsWith('/seller/dashboard')) {
-      import('@/components/dashboard/SellerLayout').then((mod) => setLayout(() => mod.default));
+      import('@/components/dashboard/SellerLayout')
+        .then((mod) => setLayout(() => mod.default))
+        .finally(() => setLoadingLayout(false));
     } else if (router.pathname.startsWith('/buyer/dashboard')) {
-      import('@/components/dashboard/BuyerLayout').then((mod) => setLayout(() => mod.default));
+      import('@/components/dashboard/BuyerLayout')
+        .then((mod) => setLayout(() => mod.default))
+        .finally(() => setLoadingLayout(false));
     } else {
       setLayout(null);
+      setLoadingLayout(false);
     }
   }, [router.pathname]);
 
-  if (layout) {
-    const Layout = layout;
-    return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    );
+  if (loadingLayout) {
+    return <div>Loading layout...</div>;
   }
 
-  return <Component {...pageProps} />;
+  const PageContent = <Component {...pageProps} />;
+
+  return (
+    <AuthProvider
+      authType={'localstorage'}
+      authName={'_auth'}
+     
+    >
+      {Layout ? <Layout>{PageContent}</Layout> : PageContent}
+    </AuthProvider>
+  );
 }
